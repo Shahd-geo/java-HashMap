ae66ec1451acff60fa291f6ac286d2bc3893d372
Shahd <shahad9mubarak@gmail.com>
2026-04-15 08:37:35 +0400

    Task 2: Retrieve and Display Values from a Java HashMap

--- src/TASK1.java
+++ src/TASK1.java
@@ -1,19 +1,18 @@
 import java.util.*;
-public class TASK1 {
-    public static void main(String[] args) {
-            HashMap<String, Integer> EmployeeHashMapDemo = new HashMap<>();
-            EmployeeHashMapDemo.put("Ahmad", 1456);
-            EmployeeHashMapDemo.put("Ali", 1978);
-            EmployeeHashMapDemo.put("Asad", 1908);
-            EmployeeHashMapDemo.put("Sara", 7856);
-            EmployeeHashMapDemo.put("Shahd", 2786);
-            System.out.println(EmployeeHashMapDemo);
-            System.out.println(EmployeeHashMapDemo.keySet());
-            System.out.println(EmployeeHashMapDemo.values());
-           EmployeeHashMapDemo.put("Qabas", 7890);
-           System.out.println(EmployeeHashMapDemo.keySet());
-           System.out.println(EmployeeHashMapDemo.values());
+//public class TASK1 {
+          // EmployeeHashMapDemo.put("Ali", 1978);
+          //  EmployeeHashMapDemo.put("Asad", 1908);
+           // EmployeeHashMapDemo.put("Sara", 7856);
+           // EmployeeHashMapDemo.put("Shahd", 2786);
+           // System.out.println(EmployeeHashMapDemo);
+           // System.out.println(EmployeeHashMapDemo.keySet());
+           // System.out.println(EmployeeHashMapDemo.values());
+           //EmployeeHashMapDemo.put("Qabas", 7890);
+           //System.out.println(EmployeeHashMapDemo.keySet());
+           //System.out.println(EmployeeHashMapDemo.values());
 
 
-    }
-}
+//}
+//}// public static void main(String[] args) {
+           // HashMap<String, Integer> EmployeeHashMapDemo = new HashMap<>();
+            //EmployeeHashMapDemo.put("Ahmad", 1456);
