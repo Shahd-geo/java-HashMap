bdc8cc757a3281e1060d71562c9003f986f9b7db
Shahd <shahad9mubarak@gmail.com>
2026-04-15 09:51:09 +0400

    Task 13: Store and Manage Student Marks in a Java HashMap

---
+++ src/CountryCapitalDemo.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class CountryCapitalDemo {
+    static void main(String[] args) {
+        HashMap<String,Integer> CountryCapitalDemo=new HashMap<>();
+        ProductPriceDemo.put("Apple",12);
+        ProductPriceDemo.put("Orange",55);
+        ProductPriceDemo.put("Ipad",100);
+        ProductPriceDemo.put("Phone",300);
+        ProductPriceDemo.put("Bananaa",7);
+        System.out.println(ProductPriceDemo);
+    }
+}
