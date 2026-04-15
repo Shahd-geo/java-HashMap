359a5316f599a4c28c4f492912aefb70ba4157af
Shahd <shahad9mubarak@gmail.com>
2026-04-15 08:25:44 +0400

    Task 1: Create and Add Elements to a Java HashMap

---
+++ src/TASK1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class TASK1 {
+    public static void main(String[] args) {
+            HashMap<String, Integer> EmployeeHashMapDemo = new HashMap<>();
+            EmployeeHashMapDemo.put("Ahmad", 1456);
+            EmployeeHashMapDemo.put("Ali", 1978);
+            EmployeeHashMapDemo.put("Asad", 1908);
+            EmployeeHashMapDemo.put("Sara", 7856);
+            EmployeeHashMapDemo.put("Shahd", 2786);
+            System.out.println(EmployeeHashMapDemo);
+            System.out.println(EmployeeHashMapDemo.keySet());
+            System.out.println(EmployeeHashMapDemo.values());
+           EmployeeHashMapDemo.put("Qabas", 7890);
+           System.out.println(EmployeeHashMapDemo.keySet());
+           System.out.println(EmployeeHashMapDemo.values());
+
+
+    }
+}
